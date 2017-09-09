b65c763886c332e2e054872e43c74eee375acdd7
Froax <robin.defeijter@student.hu.nl>
2017-09-09 23:45:03 +0200

    Initial commit

---
+++ Practice_excercise_2.py
@@ -0,0 +1,33 @@
+
+
+#Lesson 2
+
+#Practice exercise 2_1
+
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+sorted(letters)
+a = letters.count('A')
+b = letters.count('B')
+c = letters.count('C')
+
+list_1 = [a, b, c]
+print(list_1)
+
+
+#Practice exercise 2_2
+
+#Practice exercise 2_3
+
+print(0 == (1 == 2))
+
+print(2 + 3 == 4 + 5 == 7)
+
+print(1 < -1 == 3 > 4)
+
+
+#Practice exercise 2_4
+
+uurloon = int(input('Hoeveel verdien je per uur?'))
+tot_uur = int(input('Hoeveel uur heb je gewerkt?'))
+
+print("geld totaal:", uurloon * tot_uur)
